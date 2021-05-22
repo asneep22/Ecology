e2f184cd5e304d8295624dfa190a1edc34f46a9a
asneep22 <84315546+asneep22@users.noreply.github.com>
2021-05-22 15:13:22 +0500

    Update 22.05.2021 15:12
    Убрал баг при котором подбиралось сразу 2 еденицы мусора

--- EcologyGame/Assets/scripts/layer_changer.cs
+++ EcologyGame/Assets/scripts/layer_changer.cs
@@ -7,10 +7,13 @@ public class layer_changer : MonoBehaviour
     // Start is called before the first frame update
     private GameObject player;
 
-    public float distance;
+    private Vector3 start_position;
+
+    public float change_distance;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        start_position = transform.position;
     }
 
     // Update is called once per frame
@@ -18,10 +21,10 @@ public class layer_changer : MonoBehaviour
     {
         if (player.transform.position.y > transform.position.y + 0.05f)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 0.2f);
+            transform.position = start_position;
         } else
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + 0.2f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + change_distance);
         }
     }
 }

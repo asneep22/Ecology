01005d99adde980c8efc858abd5c4f005a722790
asneep22 <84315546+asneep22@users.noreply.github.com>
2021-05-24 16:32:16 +0500

    Update 24.05.2021 16:32
    Восстановил первый уровень и добавил на него мусор. Немного изменил иерархию меню из-за этого появился баг, из-за которого не работает крестик справа в меню. Выход из меню на Esc. немного исправил анимацию статуса загрязнения (Слевавверху). Добавил скрипт, при котором статус растет с увеличением загрязнения. Добавил в интерфейс картинку того, что тащит ГГ (Справа вверху).

--- EcologyGame/Assets/scripts/trash_object_in_the_hand_interfce.cs
+++ EcologyGame/Assets/scripts/trash_object_in_the_hand_interfce.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class trash_object_in_the_hand_interfce : MonoBehaviour
 {
+
+
+    private SpriteRenderer sp_renderer_trash_interface;
+    private SpriteRenderer sp_renderer_trash_in_the_hand;
+
+    private GameObject player;
+    private Transform trash_empty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        trash_empty = player.transform.GetChild(0);
+
+        sp_renderer_trash_interface = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (trash_empty.childCount == 1)
+        {
+
+            Transform trash_in_the_hand = trash_empty.transform.GetChild(0);
+            sp_renderer_trash_in_the_hand = trash_in_the_hand.GetComponent<SpriteRenderer>();
+            sp_renderer_trash_in_the_hand.size = new Vector3(0.7f, 0.7f, 0.7f);
+            sp_renderer_trash_interface.sprite = sp_renderer_trash_in_the_hand.sprite;
+        }
+        else if (trash_empty.childCount == 0)
+        {
+            sp_renderer_trash_interface.sprite = null;
+        }
+
     }
 }

555a547350db0a0e14e3656c96cd459173bb414e
asneep22 <84315546+asneep22@users.noreply.github.com>
2021-05-26 15:31:51 +0500

    Update 26.05.2021 15:28
    Сделал скрипт для сдачи мусора уникальными, то есть, для каждого типа контейнеров подойдет один скрипт. Так же добавил анимации появления тектса, если игрок сдает мусор в соотвествующие контейнеры. То есть, если сдал мусор в правильный контейнер появляется текст тип молодец и т.п. Так же и наоборот. Так же теперь статус загрязнения обновляется в зависимости от количесвта загрязнения. Чем больше, тем больше будет статус и наоборот

--- EcologyGame/Assets/scripts/tank_behaviour_green.cs
+++ EcologyGame/Assets/scripts/tank_behaviour_green.cs
@@ -1,29 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;    
+using TMPro;
 
 public class tank_behaviour_green : MonoBehaviour
 {
     private GameObject player;
+
     public TextMeshPro text;
+
 
     private GameObject trash_empty;
     private Transform trash_in_the_hand;
 
     private stretch_trash_status sts;
 
-    private int fill_tank_status = 0;
 
+    [Header("tank_status")]
+    private int fill_tank_status = 0;
     public float activity_distance;
     public int fill_tank_max = 5;
+    public string trash_type;
+
+    [Header("animation")]
+    public bool text_animate = false;
+    public List<string> anim_good_text_list = new List<string>();
+    public List<string> anim_bad_text_list = new List<string>();
+    public float anim_speed;
+
+    public bool penalty;
     // Start is called before the first frame update
     void Start()
     {
+        anim_good_text_list.AddRange(new string[] { "круто", "отлично", "молодец!","так держать!" });
+        anim_bad_text_list.AddRange(new string[] { "плохо", "внимательней", "не сюда!" });
         player = GameObject.FindGameObjectWithTag("Player");
         trash_empty = GameObject.FindGameObjectWithTag("trash_empty");
         sts = GameObject.FindGameObjectWithTag("stretch_status_element").GetComponent<stretch_trash_status>();
-        text.enabled = false;
+        text.text = "";
+        text.rectTransform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -34,16 +49,53 @@ public class tank_behaviour_green : MonoBehaviour
             trash_in_the_hand = GameObject.FindGameObjectWithTag("trash_empty").transform.GetChild(0);
         }
 
-        if (distance < activity_distance && Input.GetKey(KeyCode.E) && trash_in_the_hand != null && fill_tank_max > fill_tank_status)
+        if (distance < activity_distance && Input.GetKey(KeyCode.F) && trash_in_the_hand != null && fill_tank_max > fill_tank_status)
         {
-            fill_tank_status++;
-            Destroy(trash_in_the_hand.gameObject);
-            sts.trash.Remove(trash_in_the_hand.gameObject);
+
+            if (trash_in_the_hand.CompareTag(trash_type ))
+            {
+                fill_tank_status++;
+                Destroy(trash_in_the_hand.gameObject);
+                sts.trash.Remove(trash_in_the_hand.gameObject);
+                text.color = new Color32(101, 240, 105,255);
+                text.text = anim_good_text_list[Random.Range(0, anim_good_text_list.Count)];
+                text_animate = true;
+            } else
+            {
+                fill_tank_status++;
+                Destroy(trash_in_the_hand.gameObject);
+                sts.trash.Remove(trash_in_the_hand.gameObject);
+                text.color = new Color32(252, 45, 45 ,255);
+                text.text = anim_bad_text_list[Random.Range(0, anim_bad_text_list.Count)];
+                text_animate = true;
+            }
 
         } else if (fill_tank_status >= fill_tank_max)
         {
-            text.enabled = true;
+            text.color = Color.white;
+            text.text = "заполнено";
+            text_animate = true;
 
+        }
+
+        //Анимация текста
+        
+        if (text.text != "заполнено") {
+            if (text_animate && text.rectTransform.localScale.x < 0.97)
+            {
+                float scale = Mathf.Lerp(text.rectTransform.localScale.x, 1, Time.deltaTime * anim_speed * 1.2f);
+                text.rectTransform.localScale = new Vector3(scale, scale, text.rectTransform.localScale.z);
+            }
+            else
+            {
+                float scale = Mathf.Lerp(text.rectTransform.localScale.x, 0, Time.deltaTime * anim_speed * 1.2f);
+                text.rectTransform.localScale = new Vector3(scale, scale, text.rectTransform.localScale.z);
+                text_animate = false;
+            }
+        } else
+        {
+            float scale = Mathf.Lerp(text.rectTransform.localScale.x, 1, Time.deltaTime * anim_speed);
+            text.rectTransform.localScale = new Vector3(scale, scale, text.rectTransform.localScale.z);
         }
     }
 }

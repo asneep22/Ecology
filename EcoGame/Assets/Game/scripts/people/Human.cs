495ffcfbe49997490def7d2c4abf66d5771a40db
asneep22 <buefyf222@mail.ru>
2022-02-09 09:21:17 +0300

    UPD

--- EcoGame/Assets/Game/scripts/people/Human.cs
+++ EcoGame/Assets/Game/scripts/people/Human.cs
@@ -30,6 +30,7 @@ public class Human : MonoBehaviour
 
         _speed = Random.Range(_min_speed_m_per_sec, _max_speed_m_per_sec);
 
+        transform.localScale = (move_target.position.x - transform.position.x > 0) ? transform.localScale : new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         StartCoroutine(Try_drop_trash());
     }
 
